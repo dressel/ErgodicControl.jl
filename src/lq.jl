1fb19db8618fcaa67363448639eff4dc0ac7814b
Louis Dressel <dressel@stanford.edu>
2017-02-16 17:51:29 -0800

    correcting minor matrix error in lqr

--- src/lq.jl
+++ src/lq.jl
@@ -14,15 +14,15 @@ function LQ(A::MF, B::MF, a::MF, b::MF, Q::MF, R::MF, N::Int)
 
 	# Solely for LQ
 	r = Array(Vector{Float64}, N+1)
-	r[N+1] = zeros(2)
+	r[N+1] = zeros(size(B,1))
 	C = Array(Vector{Float64}, N)
 
 	# Sweep from the back
 	for n = (N-1):-1:0
 		G[n+1] = R + (B' * P[n+1+1] * B)
-		K[n+1] = inv(G[n+1]) * B * P[n+1+1] * A
+		K[n+1] = inv(G[n+1]) * B' * P[n+1+1] * A
 		P[n+1] = Q + (A' * P[n+1+1] * A) - (K[n+1]' * G[n+1] * K[n+1])
-		r[n+1] = (A'-K[n+1]'*B')r[n+1+1] + .5*a[:,n+1] - .5*K[n+1]*b[:,n+1]
+		r[n+1] = (A'-K[n+1]'*B')r[n+1+1] + .5*a[:,n+1] - .5*K[n+1]'*b[:,n+1]
 		C[n+1] = inv(G[n+1]) * (B'*r[n+1+1] + 0.5*b[:,n+1])
 	end
 
@@ -33,7 +33,7 @@ end
 function apply_LQ_gains(A::MF, B::MF, K::Vector{MF}, C::VV_F)
 	N = length(K)
 
-	z = [zeros(2)]
+	z = [zeros(size(B,1))]
 	v = Array(Vector{Float64}, 0)
 	for n = 1:N
 		push!(v, -K[n]*z[n] - C[n])

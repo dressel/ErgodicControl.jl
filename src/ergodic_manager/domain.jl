76774256f54953611a2a90b85ef1b4c1fe001582
Louis Dressel <dressel@stanford.edu>
2017-03-06 15:38:11 -0800

    added Domain constructors

--- src/ergodic_manager/domain.jl
+++ src/ergodic_manager/domain.jl
@@ -1,23 +1,25 @@
 ######################################################################
 # domain.jl
 #
-# experimental, not really in use yet
+# TODO: allow creation from tuples as well 
 ######################################################################
 export Domain
-type Domain
-	mins::Vector{Float64}
-	maxes::Vector{Float64}
-	cells::Vector{Int}
-	lengths::Vector{Float64}
-	cell_lengths::Vector{Float64}
-	num_dims::Int
-	cell_size::Float64
 
-	function Domain(mins::VF, maxes::VF, cells::Vector{Int})
-		println("Making domain!!!")
-		# TODO: check that all the dimensions match
+type Domain
+	# user provided
+	mins::Vector{Float64}			# minimum value per dimension
+	maxes::Vector{Float64}			# maximum value per dimension
+	cells::Vector{Int}				# number of cells per dimension
+
+	# calculated and used internally
+	lengths::Vector{Float64}		# maxes - mins
+	cell_lengths::Vector{Float64}	# length of cell in each dimension
+	num_dims::Int					# number of dimensions
+	cell_size::Float64				# size (area, volume, etc) of a cell
+
+	function Domain(mins, maxes, cells::Vector{Int})
 		num_dims = length(mins)
-		@assert num_dims == length(mins)
+		@assert num_dims == length(maxes)
 		@assert num_dims == length(cells)
 		@assert num_dims > 0
 
@@ -40,7 +42,24 @@ type Domain
 		end
 		return d
 	end
+
+	# can just pass an int for num_cells too
+	function Domain(mins, maxes, num_cells::Int)
+		n = length(mins)
+		return Domain(mins, maxes, num_cells*ones(Int, n))
+	end
+
+	# Allow user to only give maxes
+	function Domain(maxes, bins::Vector{Int}) 
+		return Domain(zeros(length(maxes)), maxes, bins)
+	end
+
+	function Domain(maxes, num_cells::Int) 
+		return Domain(zeros(length(maxes)), maxes, num_cells)
+	end
+
 end
+
 
 x_min(domain::Domain) = domain.mins[1]
 y_min(domain::Domain) = domain.mins[2]

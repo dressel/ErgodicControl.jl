315c806ce0b0aff986ee9b1701987df57d10fdd2
Louis Dressel <dressel@stanford.edu>
2017-07-20 09:56:51 -0700

    minor changes to keep up with some changes

--- src/trajectory_generation/info.jl
+++ src/trajectory_generation/info.jl
@@ -19,10 +19,14 @@ If you spend `h` time there, it is equivalent to `h*D/(h*N) = D/N`
 
 Returns total info picked up (a scalar value).
 """
-function collect_info(em::ErgodicManager, traj::VVF; steps=0)
+function collect_info(em::ErgodicManager, traj::VVF; steps=0, d_rate=0.0)
 	N = length(traj) - 1
 	D = sum(em.phi)
-	d_rate = D/(N+1)
+
+	# if user has not specified decay rate, just set it to normal
+	if d_rate == 0.0
+		d_rate = D/(N+1)
+	end
 
 	total_info = 0.0
 	if steps != 0
@@ -61,7 +65,7 @@ end
 """
 `optimal_info(em, N)`
 
-Returns optimal information gathered
+Returns optimal information gathered.
 """
 function optimal_info(em::ErgodicManager, N::Int)
 	d_rate = sum(em.phi) / (N+1)
@@ -80,20 +84,18 @@ end
 
 
 # create trajectory to do the above
-# TODO: this should maybe go somewhere else
 export optimal_traj
 function optimal_traj(em::ErgodicManager, tm::TrajectoryManager)
-	d_rate = sum(em.phi)/tm.N
-	num_cells = em.bins*em.bins
+	d_rate = sum(em.phi) / (tm.N+1)
 	total_info = 0.0
 	xd = Array(Vector{Float64}, tm.N+1)
 	xd[1] = deepcopy(tm.x0)
 	temp_phi = deepcopy(em.phi)
-	size_tuple = (em.bins, em.bins)
+	size_tuple = (x_cells(em), y_cells(em))
 	for n = 1:tm.N
 		bi = indmax(temp_phi)
 		xi, yi = ind2sub(size_tuple, bi)
-		xd[n+1] = [(xi-0.5)*em.cell_size, (yi-0.5)*em.cell_size]
+		xd[n+1] = [(xi-0.5)*x_size(em), (yi-0.5)*y_size(em)]
 		temp_phi[bi] -= min(temp_phi[bi], d_rate)
 	end
 	ud = compute_controls(xd, tm.h)

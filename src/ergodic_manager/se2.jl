1d6b4058f459beb262d61870613ced18c9c72c67
dressel <louisdressel@gmail.com>
2018-03-17 10:38:23 -0700

    huge speed improvements to se2

--- src/ergodic_manager/se2.jl
+++ src/ergodic_manager/se2.jl
@@ -1,9 +1,9 @@
 ######################################################################
-# se48.jl
+# se2.jl
 #
-# copy of se4.jl
-# but, we try caching more intelligently
 ######################################################################
+
+const enum = enumerate
 
 
 export ErgodicManagerSE2
@@ -20,30 +20,33 @@ Valid `example_name` entries are:
 """
 mutable struct ErgodicManagerSE2 <: ErgodicManager
     domain::Domain
-    M::Int
-    N::Int
+
+    M::UnitRange{Int}               # Fourier coefficient indices
+    N::UnitRange{Int}
     P::Int
+
     phi::Array{Float64,3}				# spatial distribution
     phik::Array{Complex{Float64},3}		# spatial Fourier coefficients
-    Lambda::Array{Float64,3}			# constants 
 
-    bessel_cache::Array{Complex{Float64},6}
+    Lambda::Array{Float64,3}			# constants 
+    cache::Array{Complex{Float64},6}    # stores values of F_mnp(x,y,z)
 
 
     function ErgodicManagerSE2(d::Domain, phi::Array{Float64,3}, K::Int=5)
         em = new()
         em.domain = deepcopy(d)
-        em.M = K
-        em.N = K
+
+        em.M = 0:K
+        em.N = 0:K
         em.P = K
+
         em.phi = deepcopy(phi)
-        em.phik = zeros(em.M+1, em.N+1, em.P)
-        em.Lambda = zeros(em.M+1, em.N+1, em.P)
+        em.phik = zeros(length(em.M), length(em.N), em.P)
+        em.Lambda = zeros(length(em.M), length(em.N), em.P)
 
         tic()
-        em.bessel_cache = make_bessel_cache(em)
-        rar = toq()
-        println("cache time = ", rar)
+        cache!(em)
+        println("SE2 cache time = ", round(toq(),3), " seconds")
 
         Lambda!(em)
         decompose!(em)
@@ -53,9 +56,11 @@ mutable struct ErgodicManagerSE2 <: ErgodicManager
     function ErgodicManagerSE2(d::Domain, K::Int=5)
         em = new()
         em.domain = deepcopy(d)
-        em.M = K
-        em.N = K
+
+        em.M = 0:K
+        em.N = 0:K
         em.P = K
+
         em.phik = zeros(em.M+1, em.N+1, em.P)
         em.Lambda = zeros(em.M+1, em.N+1, em.P)
 
@@ -64,31 +69,32 @@ mutable struct ErgodicManagerSE2 <: ErgodicManager
     end
 end
 
-function make_bessel_cache(em)
-    cache = zeros(Complex{Float64}, z_cells(em), y_cells(em), x_cells(em), em.P, em.N+1, em.M+1)
-    for m = 0:em.M, n = 0:em.N
-        for p = 1:em.P
-            for xi = 1:x_cells(em), yi = 1:y_cells(em), zi=1:z_cells(em)
-                x = x_min(em) + (xi-0.5)*x_size(em)
-                y = y_min(em) + (yi-0.5)*y_size(em)
-                z = z_min(em) + (zi-0.5)*z_size(em)
-                cache[zi,yi,xi,p,n+1,m+1] = F_mnp(m,n,p,x,y,z)
-            end
+
+# Calls to F_mnp are expensive as they rely on the bessel function.
+# Therefore, we store calls to this function in an array beforehand.
+function cache!(em::ErgodicManagerSE2)
+    n_M = length(em.M)
+    n_N = length(em.N)
+    cache = zeros(Complex{Float64}, z_cells(em), y_cells(em), x_cells(em), em.P, n_N, n_M)
+    for (mi,m) in enum(em.M), (ni,n) in enum(em.N), p = 1:em.P
+        for xi = 1:x_cells(em), yi = 1:y_cells(em), zi=1:z_cells(em)
+            x = x_min(em) + (xi-0.5)*x_size(em)
+            y = y_min(em) + (yi-0.5)*y_size(em)
+            z = z_min(em) + (zi-0.5)*z_size(em)
+            cache[zi,yi,xi,p,ni,mi] = F_mnp(m,n,p,x,y,z)
         end
     end
-    return cache
+    em.cache = cache
 end
-
 
 
 # fills the matrix Lambda_{m,n,p}
 function Lambda!(em::ErgodicManagerSE2)
-    for m = 0:em.M, n = 0:em.N, p = 1:em.P
+    for (mi,m) in enum(em.M), (ni,n) in enum(em.N), p = 1:em.P
         den_sqrt = (1.0 + m*m + n*n + p*p)
-        em.Lambda[m+1, n+1, p] = 1.0 / (den_sqrt * den_sqrt)
+        em.Lambda[mi, ni, p] = 1.0 / (den_sqrt * den_sqrt)
     end
 end
-
 
 
 ######################################################################
@@ -96,7 +102,6 @@ end
 ######################################################################
 function phi!(em::ErgodicManagerSE2, dm::VF, ds::MF)
     # first, generate d
-    #d = zeros(em.bins, em.bins, em.bins)
     d = zeros(x_cells(em), y_cells(em), z_cells(em))
     d_sum = 0.0
     for xi = 1:x_cells(em)
@@ -123,23 +128,19 @@ end
 # Here, I assume it is discrete, but I should change this...
 # TODO: maybe I should do some bounds checking?
 function decompose!(em::ErgodicManagerSE2, d::Array{Float64,3})
-    for m = 0:em.M
-        #println("m = ", m)
-        for n = 0:em.N, p = 1:em.P
-            em.phik[m+1,n+1,p] = phi_mnp(em, m, n, p, d)
-        end
+    for (mi,m) in enum(em.M), (ni,n) in enum(em.N), p = 1:em.P
+        em.phik[mi,ni,p] = phi_mnp(em, mi, ni, p, d)
     end
     em.phi = d
 end
 
 
-# iterate over the state space
-function phi_mnp(em::ErgodicManagerSE2, m::Int, n::Int, p::Int, d::Array{Float64,3})
+# computes phi_mnp by iterating over the state space
+# uses the cache to evaluate F_mnp
+function phi_mnp(em::ErgodicManagerSE2, mi::Int, ni::Int, p::Int, d::Array{Float64,3})
 	val = 0.0im
 	for xi = 1:x_cells(em), yi = 1:y_cells(em), zi = 1:z_cells(em)
-        #val += d[xi,yi,zi] * em.bessel_cache[m+1,n+1,p,xi,yi,zi]*p / (4*pi*pi)
-        #val += d[xi,yi,zi] * em.bessel_cache[m+1,n+1,p,xi,yi,zi]
-        val += d[xi,yi,zi] * em.bessel_cache[zi, yi, xi, p, n+1, m+1]
+        val += d[xi,yi,zi] * em.cache[zi, yi, xi, p, ni, mi]
 	end
 	return val * em.domain.cell_size
 end
@@ -155,16 +156,18 @@ end
 
 function decompose(em::ErgodicManagerSE2, traj::VVF)
     N = length(traj)-1
-    ck = zeros(Complex{Float64}, em.M+1, em.N+1, em.P)
-    for m = 0:em.M, n = 0:em.N, p = 1:em.P
+
+    ck = zeros(Complex{Float64}, length(em.M), length(em.N), em.P)
+
+    for (mi,m) in enum(em.M), (ni,n) in enum(em.N), p = 1:em.P
         fk_sum = 0.0im
         # now loop over time
         for i = 0:N-1
             xi = traj[i + 1]
-            fk_sum += F_mnp(m,n,p,xi[1],xi[2],xi[3])
+            fk_sum += F_mnp(m, n, p, xi[1], xi[2], xi[3])
         end
         # TODO: check that this is right
-        ck[m+1, n+1, p] = fk_sum / N
+        ck[mi, ni, p] = fk_sum / N
     end
     return ck
 end
@@ -173,23 +176,20 @@ end
 # reconstructs from Fourier coefficients in ck
 # This comes from last equation in chapter 10.3 of 
 #  Engineering Applications of Noncommutative Harmonic Analysis
-# TODO: This is a first cut. Things that I can/should probably change
-#  * probably compute M, N before P
-#  * p = 0 seems to add nothing, don't bother there
-#  * is the call to real() necessary?
+#
+# Returns an array of complex numbers.
+# Even if you expect an array of reals, there will be small complex values
+# call real(output) if you want just the real parts
 function reconstruct(em::ErgodicManagerSE2, ck::Array{Complex{Float64},3})
 
-    vals = zeros(x_cells(em), y_cells(em), z_cells(em))
-    #vals = zeros(Complex{Float64}, x_cells(em), y_cells(em), z_cells(em))
+    vals = zeros(Complex{Float64}, x_cells(em), y_cells(em), z_cells(em))
 
-    for xi = 1:x_cells(em), yi = 1:y_cells(em), zi=1:z_cells(em)
-        for m = 0:em.M, n = 0:em.N, p = 1:em.P
-            c = ck[m+1,n+1,p]
-            #f = conj(em.bessel_cache[m+1,n+1,p,xi,yi,zi])
-            f = conj(em.bessel_cache[zi, yi, xi, p, n+1, m+1])
-            vals[xi,yi,zi] += real(c * f * p)
-            #vals[xi,yi,zi] += c * f * p * p
-            #vals[xi,yi,zi] += c * f
+    # this order is much better
+    for (mi,m) in enum(em.M), (ni,n) in enum(em.N), p = 1:em.P
+        c = ck[mi, ni, p]
+        for xi = 1:x_cells(em), yi = 1:y_cells(em), zi=1:z_cells(em)
+            f = conj(em.cache[zi, yi, xi, p, ni, mi])
+            vals[xi,yi,zi] += c * f * p
         end
 	end
 	return vals

9e0ae96168b36cf38f52773a65c96dcf6477a0b7
dressel <louisdressel@gmail.com>
2018-04-03 11:21:01 -0700

    initial tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,3 +3,12 @@ using Base.Test
 
 # write your own tests here
 @test 1 == 1
+
+em = ErgodicManagerR2("single gaussian", K=5, bins=100)
+x0 = [0.4,0.1]
+N = 40
+h = 0.1
+
+tm = TrajectoryManager(x0, h, N, ConstantInitializer([0.0,0.0]))
+xd, ud = pto_trajectory(em, tm)
+

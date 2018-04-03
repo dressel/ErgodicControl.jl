207902ad667bb825e2bcda6e4a28917e92b9f0dd
dressel <louisdressel@gmail.com>
2018-04-03 16:20:34 -0700

    cleaning up main file just a bit

--- src/ErgodicControl.jl
+++ src/ErgodicControl.jl
@@ -9,27 +9,23 @@ export ErgodicManager, decompose!, reconstruct, decompose
 export ergodic_score, control_score, total_score
 export control_effort
 export centroid, covariance
-export TrajectoryManager, dynamics!, make_trajectory, sample_trajectory
+export TrajectoryManager, dynamics!, sample_trajectory
 export kmeans_trajectory, random_trajectory
 export assign_step, mean_step
-export Initializer, initialize
-export CornerConstantInitializer, GreedyInitializer, PointInitializer
-export DirectionInitializer
-export clerc_trajectory
-export cerc_trajectory
-export x_min, y_min, z_min
-export x_max, y_max, z_max
-export x_size, y_size, z_size
-export x_cells, y_cells, z_cells
 
-# to make some things easier
-# deprecation
-#typealias T2F      NTuple{2, Float64}    # x, y
-#typealias MF  Matrix{Float64}
-#typealias VMF Vector{MF}
-#typealias VF  Vector{Float64}
-#typealias VVF Vector{VF}
-#typealias VVVF Vector{VVF}
+export
+    Initializer,
+    initialize,
+    CornerConstantInitializer,
+    GreedyInitializer,
+    PointInitializer,
+    DirectionInitializer
+
+export
+    x_min, y_min, z_min,
+    x_max, y_max, z_max,
+    x_size, y_size, z_size,
+    x_cells, y_cells, z_cells
 
 # to make some things easier
 const T2F = NTuple{2, Float64}    # x, y
@@ -64,10 +60,5 @@ include("trajectory_manager/descender.jl")
 
 # trajectory generation
 include("trajectory_generation/trajectory_generation.jl")
-
-# include visuals
-#include("visuals/plots.jl")
-#include("visuals/gif.jl")
-
 
 end # module
